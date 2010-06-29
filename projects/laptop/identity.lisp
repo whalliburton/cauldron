d5b2c0e2ce786518090fef121ca4f7d38f09ed5e
William Halliburton <whalliburton@gmail.com>
2010-06-29 15:20:22 -0700

    Add laptop/identity.

---
+++ projects/laptop/identity.lisp
@@ -0,0 +1,22 @@
+;; identity.lisp
+
+(in-package :laptop)
+
+(defclass ssh-identity ()
+  ((key-size :initarg :key-size)
+   (fingerprint :initarg :fingerprint)
+   (filename :initarg :filename)
+   (type :initarg :type)))
+
+(defmethod print-object ((ssh-identity ssh-identity) stream)
+  (with-slots (type filename) ssh-identity
+    (print-unreadable-object (ssh-identity stream :type t)
+      (format stream "~a ~a" type filename))))
+
+(defun identities ()
+  (mapcar (lambda (el)
+            (destructuring-bind (size fingerprint filename type) el
+              (make-instance 'ssh-identity 
+                             :key-size size :fingerprint fingerprint :filename filename :type type))) 
+          (mapcar (lambda (el) (split-sequence #\space el))
+                  (split-sequence '#\newline (run "/usr/bin/ssh-add" "-l") :remove-empty-subseqs t))))
--- projects/laptop/packages.lisp
+++ projects/laptop/packages.lisp
@@ -3,7 +3,7 @@
 (defpackage laptop
   (:use common-lisp utilities split-sequence print-table cl-fad anaphora)
   (:import-from sb-ext quit)
-  (:export thermal device devices memory-information process processes))
+  (:export thermal device devices memory-information process processes identities))
 
 
 
