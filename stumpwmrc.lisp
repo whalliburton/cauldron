145e9c46f8a1ad69906ae1bdeb7a01bcfb002e0e
William Halliburton <whalliburton@gmail.com>
2010-06-30 07:18:31 -0700

    Move stumpwmrc from home to cauldron.

---
+++ stumpwmrc.lisp
@@ -0,0 +1,181 @@
+(in-package :stumpwm)
+
+(setf *startup-message* nil)
+
+(run-shell-command "xsetroot -cursor_name left_ptr")
+
+;; Load swank.
+(load "/lisp/site-lisp/slime/swank-loader.lisp")
+(swank-loader:init)
+(defcommand swank () ()
+  (setf stumpwm:*top-level-error-action* :break)
+  (loop for try-port = 4005 then (1+ try-port)
+        while (handler-case 
+                  (progn
+                    (swank:create-server :port try-port
+                                         :style swank:*communication-style*
+                                         :dont-close t :coding-system "utf-8-unix")
+                    (message "Swank started on port ~a." try-port)
+                    nil)
+                (sb-bsd-sockets:address-in-use-error () 
+                  (warn "SWANK port ~a taken, trying the next." try-port)
+                  t))))
+(swank)
+
+(defcommand emacs-cauldron () ()
+  (run-or-raise "emacs --eval \"(connect-to-cauldron)\"" '(:class "Emacs")))
+
+(defcommand chromium () ()
+  (run-or-raise "chromium" '(:title "Chromium")))
+
+(defcommand tmux () ()
+  (run-or-raise "exec urxvt -tn rxvt-unicode256 -e tmux attach" '(:title "tmux")))
+
+(defcommand htop () ()
+  (run-or-raise "exec urxvt -e htop" '(:title "htop")))
+
+(defcommand start-terminal (&optional arg) ((:string "suffix: "))
+  (run-or-raise  (format nil "exec urxvt -name urxvt~@[-~A~]" arg) `(:instance ,arg)))
+
+(progn
+  (setf *top-map* nil *root-map* nil *help-map* nil *group-top-map* nil
+        *tile-group-top-map* nil *tile-group-root-map* nil *groups-map* nil)
+  
+  (fill-keymap 
+   *top-map*
+   (kbd "s-x") '*root-map*
+   (kbd "s-h") *help-map*
+   (kbd "F1") "emacs-cauldron"
+   (kbd "F2") "tmux"
+   (kbd "F3") "chromium"
+   (kbd "F4") "start-terminal 0"
+   (kbd "F5") "start-terminal 1"
+   (kbd "F6") "start-terminal 2"
+   (kbd "F7") "start-terminal 3"
+   (kbd "F8") "start-terminal 4"
+   (kbd "s-h") "htop"
+   (kbd "s-Up")      "move-focus up"
+   (kbd "s-Down")    "move-focus down"
+   (kbd "s-Left")    "move-focus left"
+   (kbd "s-Right")   "move-focus right"
+   (kbd "s-M-Up")    "move-window up"
+   (kbd "s-M-Down")  "move-window down"
+   (kbd "s-M-Left")  "move-window left"
+   (kbd "s-M-Right") "move-window right"
+   (kbd "s-'")  "windowlist"
+   (kbd "s-p")  "show-window-properties"
+   (kbd "s-SPC")     "pull-hidden-next"
+   (kbd "s-;")   "colon"
+   (kbd "s-:")   "eval"
+   (kbd "s-3")       "vsplit"
+   (kbd "s-2")       "hsplit"
+   (kbd "s-1")       "only")
+  
+  (fill-keymap 
+   *root-map*
+   (kbd "m")   "lastmsg"
+   (kbd "b")   "banish"
+   (kbd "!")   "exec"
+   (kbd "s-g") "abort"
+   (kbd "G")   "vgroups"
+   (kbd "g")   '*groups-map*
+   (kbd "h")   '*help-map*)
+
+  (fill-keymap 
+   *group-top-map*
+   (kbd "s-x") '*group-root-map*)
+  
+  (fill-keymap *group-root-map*
+               (kbd "s-u") "next-urgent"
+               (kbd "w")   "windows"
+               (kbd "s-w") "windows"
+               (kbd "k")   "delete"
+               (kbd "s-k") "delete"
+               (kbd "K")   "kill"
+               (kbd "'")   "select"
+               (kbd "s-N") "number"
+               (kbd "#")   "mark"
+               (kbd "F11") "fullscreen"
+               (kbd "A")   "title"
+               (kbd "i")   "info")
+  
+  (fill-keymap 
+   *tile-group-top-map*
+   (kbd "s-x") '*tile-group-root-map*)
+  
+  (fill-keymap 
+   *tile-group-root-map*
+
+   (kbd "n")       "pull-hidden-next"
+   (kbd "s-n")     "pull-hidden-next"
+   (kbd "M-n")     "next"
+   (kbd "s-M-n")   "next-in-frame"
+   (kbd "SPC")     "pull-hidden-next"
+   (kbd "s-SPC")   "pull-hidden-next"
+   (kbd "p")       "pull-hidden-previous"
+   (kbd "s-p")     "pull-hidden-previous"
+   (kbd "M-p")     "prev"
+   (kbd "s-M-p")   "prev-in-frame"
+   (kbd "W")       "place-existing-windows"
+   (kbd "s-x")     "pull-hidden-other"
+   (kbd "M-t")     "other-in-frame"
+   (kbd "s-0")     "pull 0"
+   (kbd "s-1")     "pull 1"
+   (kbd "s-2")     "pull 2"
+   (kbd "s-3")     "pull 3"
+   (kbd "s-4")     "pull 4"
+   (kbd "s-5")     "pull 5"
+   (kbd "s-6")     "pull 6"
+   (kbd "s-7")     "pull 7"
+   (kbd "s-8")     "pull 8"
+   (kbd "s-9")     "pull 9"
+   (kbd "R")       "remove"
+   (kbd "r")       "iresize"
+   (kbd "o")       "fnext"
+   (kbd "TAB")     "fnext"
+   (kbd "M-TAB")   "fother"
+   (kbd "f")       "fselect"
+   (kbd "F")       "curframe"
+   (kbd "-")       "fclear"
+   (kbd "+")       "balance-frames"
+   (kbd "l")       "redisplay"
+   (kbd "C-l")     "redisplay")
+  
+  (fill-keymap 
+   *groups-map*
+   (kbd "g")     "groups"
+   (kbd "c")     "gnew"
+   (kbd "n")     "gnext"
+   (kbd "s-n")   "gnext"
+   (kbd "SPC")   "gnext"
+   (kbd "s-SPC") "gnext"
+   (kbd "N")     "gnext-with-window"
+   (kbd "p")     "gprev"
+   (kbd "s-p")   "gprev"
+   (kbd "P")     "gprev-with-window"
+   (kbd "o")     "gother"
+   (kbd "'")     "gselect"
+   (kbd "\"")    "grouplist"
+   (kbd "m")     "gmove"
+   (kbd "M")     "gmove-marked"
+   (kbd "k")     "gkill"
+   (kbd "A")     "grename"
+   (kbd "r")     "grename")
+  
+  (fill-keymap 
+   *help-map*
+   (kbd "v") "describe-variable"
+   (kbd "f") "describe-function"
+   (kbd "k") "describe-key"
+   (kbd "c") "describe-command"
+   (kbd "w") "where-is")
+
+  (sync-keys))
+
+(banish)
+(emacs-cauldron)
+
+(run-shell-command "unclutter -idle 1 -jitter 2 -root")
+
+(load "/lisp/startup.lisp")
+
