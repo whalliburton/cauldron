32b296a15e812364dbef2d5ba045e00e4638fd02
William Halliburton <whalliburton@gmail.com>
2010-07-01 19:16:02 -0700

    Eh, need to stop pushing so quickly.

--- projects/cauldron/packages.lisp
+++ projects/cauldron/packages.lisp
@@ -1,7 +1,7 @@
 ;; packages.lisp
 
 (defpackage cauldron
-  (:use common-lisp cards laptop web music print-table iterate sb-thread)
+  (:use common-lisp cards laptop web music print-table iterate sb-thread databases)
   (:import-from sb-ext quit))
 
 
