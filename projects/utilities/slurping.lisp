344952cfc541bdb8bbdfba90b891d693d77b38b4
William Halliburton <whalliburton@gmail.com>
2010-08-19 02:53:37 -0700

    Add SLURP-LINE and fix SLURP-STREAM.
    Slurp stream was only correct when the stream was at the beginning.

--- projects/utilities/slurping.lisp
+++ projects/utilities/slurping.lisp
@@ -2,14 +2,13 @@
 
 (in-package :utilities)
 
-;; unused
 (defun slurp-stream (stream &optional (element-type '(unsigned-byte 8)))
   "Slurp all octets from STREAM. Returns a vector of ELEMENT-TYPE"
-  (let ((seq (make-array (file-length stream) :element-type element-type)))
+  (let ((seq (make-array (- (file-length stream) (file-position stream)) 
+                         :element-type element-type)))
     (read-sequence seq stream)
     seq))
 
-;; unused
 (defun slurp (filename &optional (element-type '(unsigned-byte 8)))
   "Slurp the contents of the file designated by FILENAME, returning
 a vector of ELEMENT-TYPE."
@@ -23,3 +22,7 @@ a vector of ELEMENT-TYPE."
     (iter (for line = (read-line f nil))
           (while line)
           (collect line))))
+
+(defun slurp-line (filename)
+  (with-open-file (f filename :direction :input :if-does-not-exist :error)
+    (read-line f nil)))
