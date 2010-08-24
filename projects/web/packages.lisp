e90452ec13e6c4aab17be6f16cb81f51530be5b4
William Halliburton <whalliburton@gmail.com>
2010-08-23 23:12:03 -0700

    Add documentation to the web package.
    Note that all the web functions are macros so as to not require any
    quoting.
    
    WEB> (google cat)

--- projects/web/packages.lisp
+++ projects/web/packages.lisp
@@ -5,6 +5,9 @@
   (:import-from sb-ext quit run-program)
   (:export web sr google javadoc rfc rhyme thesaurus wayback))
 
+(in-package :web)
+
+(defparameter *help-text* "Tools for accessing the World Wide Web.")
 
 
   
--- projects/web/surfraw.lisp
+++ projects/web/surfraw.lisp
@@ -9,16 +9,21 @@
   `(surfraw (list ,(format nil "~(~a~)" elviname) ,(format nil "~{~(~a~)~^ ~}" terms))))
 
 (defmacro google (&rest terms)
+  "Google search."
   `(sr google ,@terms))
 
 (defmacro javadoc (&rest terms)
+  "Javadoc search."
   `(sr javasun ,@terms))
 
 (defmacro rfc (&rest terms)
+  "RFC search."
   `(sr rfc ,@terms))
 
 (defmacro rhyme (&rest terms)
+  "Rhyming word search."
   `(sr rhyme ,@terms))
 
 (defmacro thesaurus (&rest terms)
+  "Thesaurus search."
   `(sr thesaurus ,@terms))
